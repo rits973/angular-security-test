251e7f08550ce5a05fcd5a5c935d0ba14cf10a57
Rits <ritesh.kumar@thrymr.net>
2018-11-09 19:06:59 +0530

    Code commit

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AuthLoginInfo } from './login-info';
+import { Observable } from 'rxjs';
+import { JwtResponse } from './jwt-response';
+import { SignUpInfo } from './sigup-info';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private loginUrl = 'http://localhost:8080/api/auth/signin';
+  private signupUrl = 'http://localhost:8080/api/auth/signup';
+ 
+  constructor(private http: HttpClient) {
+  }
+ 
+  attemptAuth(credentials: AuthLoginInfo): Observable<JwtResponse> {
+    return this.http.post<JwtResponse>(this.loginUrl, credentials, httpOptions);
+  }
+ 
+  signUp(info: SignUpInfo): Observable<string> {
+    return this.http.post<string>(this.signupUrl, info, httpOptions);
+  }
+}
